7f301f9e6e4b5995e3aa4d6a619542955c362908
Gisele Reis <giselealvesdosreis@gmail.com>
2025-03-29 20:56:54 -0300

    :sparkles: feat: cria tela Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,45 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { useRouter } from 'vue-router';
+import Prompt from '@/components/Input.vue';
+
+const router = useRouter();
+
+const goToAnswer = (text: string) => {
+  if (text.trim()) {
+    router.push({ path: '/answer', query: { text } });
+  }
+};
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="container">
+    <div class="welcome">
+      <img src="../components/icons/logo.svg" alt="logo">
+      <h1>Bem-Vindo(a)!</h1>
+    </div>
+    <Prompt @send="goToAnswer" />
   </main>
 </template>
+
+
+<style scoped>
+.container {
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  place-items: center; 
+}
+
+.welcome {
+  display: grid; 
+  place-items: center;
+}
+
+img {
+  width: 250px;
+}
+
+h1 {
+  font-size: 3em;
+}
+</style>
