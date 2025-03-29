e8fbd82c62b1ca6e74f65c6c211c568c22d0da77
Gisele Reis <giselealvesdosreis@gmail.com>
2025-03-29 20:53:34 -0300

    :sparkles: feat: cria componente input

---
+++ src/components/Input.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { ref, defineEmits } from 'vue';
+
+const inputText = ref('');
+const emit = defineEmits(['send']);
+
+const sendMessage = () => {
+  if (inputText.value.trim()) {
+    emit('send', inputText.value);
+    inputText.value = ''; 
+  }
+};
+</script>
+
+<template>
+  <div class="input">
+    <textarea v-model="inputText" placeholder="Digite aqui sua pergunta"></textarea>
+    <button @click="sendMessage">
+      <img src="./icons/Arrow 1.svg" alt="Enviar">
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+.input {
+    display: flex; 
+    margin-right: 4em; 
+    width: 70%; 
+    padding: 5px; 
+    padding-inline: 20px; 
+    border-radius: 12px; 
+    background-color: #454545;
+}
+
+textarea {
+    width: 100%;
+    height: 2.5em;
+    max-height: 12em; 
+    border: none; 
+    border-radius: 5px; 
+    background-color: #454545;
+    outline: none;
+    color: #f0f0f0;
+    resize: none;
+    padding: 10px;
+    scrollbar-width: none;
+}
+
+textarea::placeholder {
+    color: #f0f0f0;
+}
+
+button {
+    border: none;
+    background-color: transparent; 
+    color: #00B4F0; 
+    font-weight: bold; 
+    padding-inline: 5px;
+    cursor: pointer;
+}
+
+</style>
